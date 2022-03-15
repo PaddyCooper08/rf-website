605e932a8d44ae381bf8e33bad3e8fdfd572a49f
PaddyCooper08 <1210@rgsg.co.uk>
2022-03-15 15:54:15 +0000

    Fixed bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
+import HomePage from './components/HomePage.vue'
+</script>./components/HomePage.vue
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HomePage msg="Hello Vue 3 + Vite" />
 </template>
 
 <style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+
+</script>
+
+<template>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      Recycling Identifier
+    </a>
+  </div>
+</nav>
+  
+  
+  
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
